82027f02586868d42d9ab1f6a177fe103e86335d
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-09-25 11:33:34 -0500

    add and update dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,19 @@ module.exports = {
   mode: 'development',
   entry: path.join(__dirname, 'bundle.js'),
   output: {
-    // publicPath: '/dist/'
+    // publicPath: 'http://localhost:9000/dist/'
   },
   devServer: {
     contentBase: __dirname,
     publicPath: '/dist/',
     compress: true,
+    host: '0.0.0.0',
     port: 9000,
     hotOnly: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
-    }
+    },
+    sockHost: 'localhost'
   },
   module: {
     rules: [
